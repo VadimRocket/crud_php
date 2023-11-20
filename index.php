c9665644fb60c695c6a244bd435e5089e58983aa
VadimRocket <vmartintestng@gmail.com>
2023-11-20 05:09:27 -0800

    T1: add table to index.php

--- index.php
+++ index.php
@@ -15,17 +15,48 @@
       </div>
       <div class="row">
         <div class="col-12">
-          <button class="
-           btn btn-primary rounded-0 btn-add" 
+          <!-- Button -->
+          <button class="btn btn-primary rounded-0 btn-add" 
            data-bs-toggle="modal" 
            data-bs-target="#addCity"
           >Add city</button>
-          <!-- Modal -->
+        </div>
+        <!-- Table -->
+        <div class="table-responsive my-3">
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td colspan="2">Larry the Bird</td>
+                <td>@twitter</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
   
-  <!-- Modal add city with id addCity-->
+<!-- Modal add city with id addCity-->
 <div class="modal fade" id="addCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">

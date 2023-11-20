7c5bc923f10470dc198639825912147cb3e367d4
VadimRocket <vmartintestng@gmail.com>
2023-11-20 05:05:00 -0800

    T1: add button and 2 modals to index.php

--- index.php
+++ index.php
@@ -4,11 +4,64 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://getbootstrap.com/docs/5.3/getting-started/introduction/#cdn-links">
-    <title>CRUD php</title>
+    <title>CRUD PHP with Ajax & MySQL</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1 class="text-center h2 my-3">CRUD PHP with Ajax MySQL</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <button class="
+           btn btn-primary rounded-0 btn-add" 
+           data-bs-toggle="modal" 
+           data-bs-target="#addCity"
+          >Add city</button>
+          <!-- Modal -->
+        </div>
+      </div>
+    </div>
   
+  <!-- Modal add city with id addCity-->
+<div class="modal fade" id="addCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Add city</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <!-- Modal edit(update) city with id editCity -->
+<div class="modal fade" id="editCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>

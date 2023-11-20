77adf1556a15e9faa44571a8c7115fa221762ea7
VadimRocket <vmartintestng@gmail.com>
2023-11-20 06:42:54 -0800

    T6: include files to index.php & rebase html to views/index.tpl.php

--- index.php
+++ index.php
@@ -1,99 +1,23 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <title>CRUD PHP with Ajax & MySQL</title>
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h1 class="text-center h2 my-3">CRUD PHP with Ajax MySQL</h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <!-- Button -->
-          <button class="btn btn-primary rounded btn-add" 
-           data-bs-toggle="modal" 
-           data-bs-target="#addCity"
-          >Add city</button>
-        </div>
-        <!-- Table -->
-        <div class="table-responsive my-3">
-          <table class="table table-hover">
-            <thead class="table-dark">
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  
-<!-- Modal add city with id addCity-->
-<div class="modal fade" id="addCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Add city</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+<?php
+// Подключить файлы. $config содержит массив
+$config = require_once 'config.php';
+require_once 'functions.php';
+require_once 'classes/Db.php';
+require_once 'classes/Pagination.php';
 
-  <!-- Modal edit(update) city with id editCity -->
-<div class="modal fade" id="editCity" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+// The Db::getInstance() returns object.  The method getConnection()  connect us with DB
+$db = (Db::getInstance())->getConnection($config['db']);
+$page = $_GET['page'] ?? 1;          // текущий номер стр-цы
+$per_page = $config['per_page'];     // кол-во записей на стр-це 10
+$total = getCount('city');           // получить общее кол-во городов. int 4079
+$pagination = new Pagination($page, $per_page, $total); // create object Pagination
+/*
+ $start  - с какой записи начинать выборку. Нач-ся с 0. 
+ Например: http://localhost/crud_php/?page=3  Тогда  $start = int 20 
+*/
+$start = $pagination->get_start();        
+$cities = get_cities($start, $per_page);  // получить города как массив 10 records
+// Подключить файл
+require_once 'views/index.tpl.php';
+?>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  </body>
-</html>

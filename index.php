4b443c23bc91e4cdd117cde71a66b1ab863ccaad
VadimRocket <vmartintestng@gmail.com>
2023-11-20 04:36:11 -0800

    T1: include bootstrap 5.3 css,js to index.php

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://getbootstrap.com/docs/5.3/getting-started/introduction/#cdn-links">
     <title>CRUD php</title>
   </head>
   <body>
     <h1>Hello, world!</h1>
+  
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
